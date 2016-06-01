1eb0b3feb396bb578ab66fe5f812bac529ca58aa
sabahshariq <sabah_365@hotmail.com>
2016-06-01 10:42:46 -0400

    Adding Project to Repo

---
+++ RAM_Size/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Diagnostics;
+using System.Management;
+
+namespace ConsoleApplication3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            ObjectQuery wql = new ObjectQuery("SELECT * FROM Win32_OperatingSystem");
+            ManagementObjectSearcher searcher = new ManagementObjectSearcher(wql);
+            ManagementObjectCollection results = searcher.Get();
+
+            double res;
+
+            foreach (ManagementObject result in results)
+            {
+                res = Convert.ToDouble(result["TotalVisibleMemorySize"]);
+                double fres = Math.Round((res / (1024 * 1024)), 2);
+                Console.WriteLine("Total usable memory size: " + fres + "GB");
+                Console.WriteLine("Total usable memory size: " + res + "KB");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
